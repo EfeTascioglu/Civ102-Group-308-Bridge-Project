82d9413ad3186fd93ab9e377fe70e9c6b8b91a7a
EfeTascioglu <efetoronto@gmail.com>
2021-11-22 17:32:02 -0500

    My Changes

--- Matlab_Code_For_Github.m
+++ Matlab_Code_For_Github.m
@@ -7,8 +7,8 @@ SFD_PL = zeros(1, n);      % Initialize SFD(x)
   
 %% 1. Point Loading Analysis (SFD, BMD) 
 P = 318; 
-[SFD_PL, BMD_PL] = ApplyPL(550, P, x, SFD_PL);      % Construct SFD, BMD 
-[SFD_PL, BMD_PL] = ApplyPL(L, P, x, SFD_PL);        % Construct SFD, BMD 
+% [SFD_PL, BMD_PL] = ApplyPL(550, P, x, SFD_PL);      % Construct SFD, BMD 
+% [SFD_PL, BMD_PL] = ApplyPL(L, P, x, SFD_PL);        % Construct SFD, BMD 
   
 %% 2. Define cross-sections 
 % There are many (more elegant ways) to construct cross-section objects 
@@ -17,12 +17,13 @@ bft = [100 100 100]; % Top Flange Width
 tft = [2.54 2.54 2.54]; % Top Flange Thickness 
 hw = [100 120 100];  % Web Height 
 tw = [1.27 1.27 1.27]; % Web Thickness (Assuming 2 separate webs) 
+spacing_web = [80 80 80]; % Includes width of the Flange
 bfb = [80 80 80];  % Bottom Flange Width 
 tfb = [1.27 1.27 1.27]; % Bottom Flange Thickness 
 a = [400 400 400];   % Diaphragm Spacing 
   
 % Optional but you need to ensure that your geometric inputs are correctly implemented 
-VisualizeBridge( {CrossSectionInputs} );  
+VisualizeBridge( xc, bft, tft, hw, tw, spacing_web, bfb, tfb, a );  
   
 %% 3. Define Material Properties 
 SigT = 30; 
@@ -33,33 +34,32 @@ TauG = 2;
 mu   = 0.2; 
   
 %% 4. Calculate Failure Moments and Shear Forces 
-V_Mat = Vfail({CrossSectionInputs}, TauU); 
-V_Glue = VfailGlue({CrossSectionInputs}, TauU); 
-V_Buck = VfailBuck({CrossSectionInputs}, E, mu ); 
-  
-M_MatT = MfailMatT({CrossSectionInputs}, SigT); 
-M_MatC = MfailMatC({CrossSectionInputs}, SigC); 
-M_Buck1 = MfailBuck({CrossSectionInputs}, E, mu, 1 ); 
-M_Buck2 = MfailBuck({CrossSectionInputs}, E, mu, 2 ); 
-M_Buck3 = MfailBuck({CrossSectionInputs}, E, mu, 3 ); 
+% V_Mat = Vfail({CrossSectionInputs}, TauU); 
+% V_Glue = VfailGlue({CrossSectionInputs}, TauU); 
+% V_Buck = VfailBuck({CrossSectionInputs}, E, mu ); 
+%   
+% M_MatT = MfailMatT({CrossSectionInputs}, SigT); 
+% M_MatC = MfailMatC({CrossSectionInputs}, SigC); 
+% M_Buck1 = MfailBuck({CrossSectionInputs}, E, mu, 1 ); 
+% M_Buck2 = MfailBuck({CrossSectionInputs}, E, mu, 2 ); 
+% M_Buck3 = MfailBuck({CrossSectionInputs}, E, mu, 3 ); 
   
 %% 4.7 Calculate Failure Load 
-Pf = FailLoad(P, SFD_PL, BMD_PL, V_Mat, V_Glue, V_Buck, M_MatT, M_MatC, M_Buck1, M_Buck2, M_Buck3); 
+% Pf = FailLoad(P, SFD_PL, BMD_PL, V_Mat, V_Glue, V_Buck, M_MatT, M_MatC, M_Buck1, M_Buck2, M_Buck3); 
   
 %% Visualization 
-VisualizePL(x, P, SFD_PL, BMD_PL, V_Mat, V_Glue, V_Buck, M_MatT, M_MatC, M_Buck1, M_Buck2, M_Buck3, 
-Pf); 
+% VisualizePL(x, P, SFD_PL, BMD_PL, V_Mat, V_Glue, V_Buck, M_MatT, M_MatC, M_Buck1, M_Buck2, M_Buck3, Pf); 
   
 %% 5. Curvature, Slope, Deflections 
-Defls = Deflections(x, BMD_PL, I, E); 
+% Defls = Deflections(x, BMD_PL, I, E); 
+
 
 function [ y_bar ] = CalculateYBar (areas, distances)
     y_bar = (areas .* distances) / sum(areas)
 end
 
-function [I] = CalcI(b,h,y_bar, dists_from_centroid) %b, h, dist_from_centroid are all vectors
+function [ I ] = CalcI(b,h,y_bar, dists_from_centroid) %b, h, dist_from_centroid are all vectors
     I = sum(b*h.^3/12) + b.*h.*(dists_from_centroid-y_bar).^2 %assuming all of the components are rectangles 
-
 end 
 
 
@@ -73,55 +73,87 @@ end
 
 % Output: SFD, BMD both 1-D arrays of length n 
  
-function [  ] = VisualizeBridge( {Geometric Inputs} ) 
+function [  ] = VisualizeBridge( xc, bft, tft, hw, tw, spacing_web, bfb, tfb, a ) 
 % Optional. Provides a graphical interpretation of user geometric inputs 
- function [ {Sectional Properties} ] = SectionProperties( {Geometric Inputs} ) % Calculates important sectional properties. Including but not limited to ybar, I, Q, etc. 
-% Input: Geometric Inputs. Format will depend on user 
-% Output: Sectional Properties at every value of x. Each property is a 1-D array of length n 
- 
-function [ V_fail ] = Vfail( {Sectional Properties}, TauU ) 
-% Calculates shear forces at every value of x that would cause a matboard shear failure 
-% Input: Sectional Properties (list of 1-D arrays), TauU (scalar material property) 
-% Output: V_fail a 1-D array of length n 
-    I = {Sectional Properties}; 
-    b = {Sectional Properties}; 
-Qcent = {Sectional Properties}; 
- 
-    V_fail = TauU .* I .* b ./ Qcent;  
-end 
- function [ V_Buck ] = VfailBuck( {Sectional Properties}, E, mu )  
-% Calculates shear forces at every value of x that would cause a shear buckling failure in the web 
-% Input: Sectional Properties (list of 1-D arrays), E, mu (material property) 
-% Output: V_Buck a 1-D array of length n 
- function [ M_MatT ] = MfailMatT( {Sectional Properties}, SigT, BMD )  
-% Calculates bending moments at every value of x that would cause a matboard tension failure 
-% Input: Sectional Properties (list of 1-D arrays), SigT (material property), BMD (1-D array) 
-% Output: M_MatT a 1-D array of length n 
-[I, ybot, ytop] = {Sectional Properties}; 
-  
-for i = 1 : length(x)    
-        if BMD(i) > 0 % If the moment is positive, the tension failure will be at the bottom 
-        M_MatT(i) = SigT * I(i) / ybot(i); 
-        elseif BMD(i) < 0 % If the moment is negative, the tension failure will be at the top 
-            M_MatT(i) = -SigT * I(i) / ytop(i); 
-        end 
-    end 
-end 
- 
-function [ M_MatT ] = MfailMatC( {Sectional Properties}, SigC, BMD ) % Similar to MfailMatT 
- function [ M_Buck ] = MfailBuck( {Sectional Properties}, E, mu, BMD )  
-% Calculates bending moments at every value of x that would cause a buckling failure 
-% Input: Sectional Properties (list of 1-D arrays), E, mu (material property), BMD (1-D array) 
-% Output: M_MatBuck a 1-D array of length n 
- function [ Pf ] = FailLoad( P, SFD, BMD, V_Mat, V_Buck, M_MatT, M_MatC, M_Buck1, M_Buck2, M_Buck3 )  
-% Calculates the magnitude of the load P that will cause one of the failure mechanisms to occur 
-% Input: SFD, BMD under the currently applied points loads (P) (each 1-D array of length n) 
-%  {V_Mat, V_Glue, ... M_MatT, M_MatC, ... } (each 1-D array of length n) 
-% Output: Failure Load value Pf 
- function [] = VisualizePL(x, SFD, BMD, V_Mat, V_Buck, M_MatT, M_MatC, M_Buck1, M_Buck2,..., Pf)  
-% Plots all outputs of design process 
- function [ Defls ] = Deflections( x, BMD, I, E )  
-% Calculates deflections 
-% Input: I(1-D arrays), E (material property), BMD (1-D array) 
-% Output: Deflection for every value of x (1-D array) or for the midspan only  
+    
+    % Assuming that the maximum width of the beam will be either the top or
+    % bottom flange:
+    max_width = max(max(bft), max(bfb))
+    max_height = max(tft + tfb + hw)
+
+    %% Draw out the Elevation View
+    figure
+    for i = 1:length(xc) -1
+        rectangle('Position', [xc(i), -tft(i), xc(i+1) - xc(i),  tft(i)]) % Draw the Top Flange
+        rectangle('Position', [xc(i), -tft(i)-hw(i), xc(i+1) - xc(i),  hw(i)]) % Draw the Web
+        rectangle('Position', [xc(i), -tft(i)-hw(i)-tfb(i), xc(i+1) - xc(i),  tfb(i)]) % Draw the Bottom Flange
+    end
+    axis([(xc(end) - xc(1)) * (-0.1), (xc(end) - xc(1)) * 1.1, max_height * (-5), max_height * 0.3])
+    title("Elevation view, with x = 0 representing the top of the deck")
+
+    %% Draw out the cross-section in each interval    
+    for i = 1:length(xc) -1
+        figure
+        rectangle('Position', [0, hw(i)+tfb(i), bft(i),  tft(i)]) % Draw the Top Flange
+        rectangle('Position', [(bft(i) - bfb(i)) / 2, 0, bfb(i),  tfb(i)]) % Draw the Bottom Flange
+        rectangle('Position', [((max_width / 2) - (spacing_web(i) / 2)), tfb(i), tw(i), hw(i)]) % Draw the Left-Side of the Web
+        rectangle('Position', [((max_width / 2) + (spacing_web(i) / 2))- tw(i), tfb(i), tw(i), hw(i)]) % Draw the Right-Side of the Web
+        
+        axis([max_width * (-0.1), max_width * 1.1, max_height * (-0.1), max_height * 1.1])
+        title("Cross Section between: " + xc(i) + " - " + xc(i+1))
+    end
+end
+
+
+
+%  function [ {Sectional Properties} ] = SectionProperties( {Geometric Inputs} ) % Calculates important sectional properties. Including but not limited to ybar, I, Q, etc. 
+% % Input: Geometric Inputs. Format will depend on user 
+% % Output: Sectional Properties at every value of x. Each property is a 1-D array of length n 
+%  
+% function [ V_fail ] = Vfail( {Sectional Properties}, TauU ) 
+% % Calculates shear forces at every value of x that would cause a matboard shear failure 
+% % Input: Sectional Properties (list of 1-D arrays), TauU (scalar material property) 
+% % Output: V_fail a 1-D array of length n 
+%     I = {Sectional Properties}; 
+%     b = {Sectional Properties}; 
+% Qcent = {Sectional Properties}; 
+%  
+%     V_fail = TauU .* I .* b ./ Qcent;  
+% end 
+%  function [ V_Buck ] = VfailBuck( {Sectional Properties}, E, mu )  
+% % Calculates shear forces at every value of x that would cause a shear buckling failure in the web 
+% % Input: Sectional Properties (list of 1-D arrays), E, mu (material property) 
+% % Output: V_Buck a 1-D array of length n 
+%  function [ M_MatT ] = MfailMatT( {Sectional Properties}, SigT, BMD )  
+% % Calculates bending moments at every value of x that would cause a matboard tension failure 
+% % Input: Sectional Properties (list of 1-D arrays), SigT (material property), BMD (1-D array) 
+% % Output: M_MatT a 1-D array of length n 
+% [I, ybot, ytop] = {Sectional Properties}; 
+%   
+% for i = 1 : length(x)    
+%         if BMD(i) > 0 % If the moment is positive, the tension failure will be at the bottom 
+%         M_MatT(i) = SigT * I(i) / ybot(i); 
+%         elseif BMD(i) < 0 % If the moment is negative, the tension failure will be at the top 
+%             M_MatT(i) = -SigT * I(i) / ytop(i); 
+%         end 
+%     end 
+% end 
+%  
+% function [ M_MatT ] = MfailMatC( {Sectional Properties}, SigC, BMD ) % Similar to MfailMatT 
+%  function [ M_Buck ] = MfailBuck( {Sectional Properties}, E, mu, BMD )  
+% % Calculates bending moments at every value of x that would cause a buckling failure 
+% % Input: Sectional Properties (list of 1-D arrays), E, mu (material property), BMD (1-D array) 
+% % Output: M_MatBuck a 1-D array of length n 
+%  function [ Pf ] = FailLoad( P, SFD, BMD, V_Mat, V_Buck, M_MatT, M_MatC, M_Buck1, M_Buck2, M_Buck3 )  
+% % Calculates the magnitude of the load P that will cause one of the failure mechanisms to occur 
+% % Input: SFD, BMD under the currently applied points loads (P) (each 1-D array of length n) 
+% %  {V_Mat, V_Glue, ... M_MatT, M_MatC, ... } (each 1-D array of length n) 
+% % Output: Failure Load value Pf 
+%  function [] = VisualizePL(x, SFD, BMD, V_Mat, V_Buck, M_MatT, M_MatC, M_Buck1, M_Buck2,..., Pf)  
+% % Plots all outputs of design process 
+%  function [ Defls ] = Deflections( x, BMD, I, E )  
+% % Calculates deflections 
+% % Input: I(1-D arrays), E (material property), BMD (1-D array) 
+% % Output: Deflection for every value of x (1-D array) or for the midspan only  
+
 
